817b3adc648a9cbcdc7fd4c8457abca0aaa2254f
demonsh <dmytro.sukhiy@gmail.com>
2021-12-15 00:13:02 +0200

    refactor auth.circom testvector generator

--- testvectorgen/go/auth/main.go
+++ testvectorgen/go/auth/main.go
@@ -2,64 +2,68 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	core "github.com/iden3/go-iden3-core"
 	"github.com/iden3/go-merkletree-sql"
 	"github.com/iden3/go-merkletree-sql/db/memory"
 	"math/big"
+	"test/utils"
 )
 
+/**
+auth.circom
+
+Generate test vectors for auth.circom
+*/
 func main() {
 	fmt.Println("\n-------\nauth.circom test vector:")
 
 	inputs := make(map[string]string)
 	ctx := context.Background()
 
+	challenge := new(big.Int).SetInt64(12345)
 	privKHex := "21a5e7321d0e2f3ca1cc6504396e6594a2211544b08c206847cdee96f832421a"
 
-	// Extract pubKey
-	key, X, Y := ExtractPubXY(privKHex)
-	inputs["BBJAx"] = X.String()
-	inputs["BBJAy"] = Y.String()
+	// extract pubKey
+	key, X, Y := utils.ExtractPubXY(privKHex)
 
+	// init claims tree
 	claimsTree, err := merkletree.NewMerkleTree(ctx, memory.NewMemoryStorage(), 4)
-	exitOnError(err)
+	utils.ExitOnError(err)
 
-	authClaim, err := AuthClaimFromPubKey(X, Y)
-	exitOnError(err)
+	// create auth claim
+	authClaim, err := utils.AuthClaimFromPubKey(X, Y)
+	utils.ExitOnError(err)
 
+	// add auth claim to cMT
 	entry := authClaim.TreeEntry()
 	hi, hv, err := entry.HiHv()
-	exitOnError(err)
+	utils.ExitOnError(err)
 	claimsTree.Add(ctx, hi.BigInt(), hv.BigInt())
 
-	inputs["BBJClaimClaimsTreeRoot"] = claimsTree.Root().BigInt().String()
+	// sign challenge
+	decompressedSig, err := utils.SignBBJJ(key, challenge.Bytes())
+	utils.ExitOnError(err)
 
-	challenge := new(big.Int).SetInt64(12345)
-	inputs["challenge"] = challenge.String()
-
-	decompressedSig, err := SignatureInputs(key, challenge.Bytes())
-	exitOnError(err)
-
-	inputs["challengeSignatureR8x"] = decompressedSig.R8.X.String()
-	inputs["challengeSignatureR8y"] = decompressedSig.R8.Y.String()
-	inputs["challengeSignatureS"] = decompressedSig.S.String()
-
+	// create new identity
 	identifier, err := core.CalculateGenesisID(claimsTree.Root())
-	exitOnError(err)
-
-	inputs["id"] = identifier.BigInt().String()
+	utils.ExitOnError(err)
 
 	// calculate current state
 	currentState, err := merkletree.HashElems(claimsTree.Root().BigInt(),
 		merkletree.HashZero.BigInt(), merkletree.HashZero.BigInt())
-	exitOnError(err)
+	utils.ExitOnError(err)
 
+	inputs["id"] = identifier.BigInt().String()
+	inputs["BBJAx"] = X.String()
+	inputs["BBJAy"] = Y.String()
+	inputs["BBJClaimClaimsTreeRoot"] = claimsTree.Root().BigInt().String()
+	inputs["challenge"] = challenge.String()
+	inputs["challengeSignatureR8x"] = decompressedSig.R8.X.String()
+	inputs["challengeSignatureR8y"] = decompressedSig.R8.Y.String()
+	inputs["challengeSignatureS"] = decompressedSig.S.String()
 	inputs["state"] = currentState.BigInt().String()
 
-	json, err := json.Marshal(inputs)
-	exitOnError(err)
-	fmt.Println(string(json))
+	utils.PrintMap(inputs)
 
 }
