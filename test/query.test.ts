f71b2053157dac43eafd62a80f334a686d5fe318
demonsh <dmytro.sukhiy@gmail.com>
2021-12-28 14:42:38 +0200

    fix test description

--- test/query.test.ts
+++ test/query.test.ts
@@ -58,7 +58,7 @@ describe("Test simpleQuery",  function() {
     });
 
     describe("#LessThan", function (){
-        it("#LessThan - 10 > 11", async () => {
+        it("#LessThan - 10 < 11", async () => {
             let w = await circuit.calculateWitness({
                 in: "10",
                 operator: LESS,
@@ -80,7 +80,7 @@ describe("Test simpleQuery",  function() {
             await circuit.assertOut(w1, {out: 0});
         });
 
-        it("#LESS - 9 < 10 ", async () => {
+        it("#LESS - 10 < 9 ", async () => {
             const w2 = await circuit.calculateWitness({
                 in: "10",
                 operator:  LESS,
