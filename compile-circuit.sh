6e90f4fd1a6530216aa5f440c9ad8d54420feabd
Oleksandr Brezhniev <oleksandr.brezhniev@gmail.com>
2023-08-23 21:48:47 +0100

    Added testvector generator for additional state transition for the user. Faster compilation of circuits and fix of old snarkjs usage because of PATH modification.

--- compile-circuit.sh
+++ compile-circuit.sh
@@ -33,12 +33,14 @@ compile_and_ts() {
     ENTROPY2=$(head -c 1024 /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | head -c 128)
     ENTROPY3=$(head -c 1024 /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | head -c 128)
 
-    time snarkjs zkey contribute circuit_0000.zkey circuit_0001.zkey --name="1st Contribution" -v -e="$ENTROPY1"
-    time snarkjs zkey contribute circuit_0001.zkey circuit_0002.zkey --name="2nd Contribution" -v -e="$ENTROPY2"
-    time snarkjs zkey contribute circuit_0002.zkey circuit_0003.zkey --name="3rd Contribution" -v -e="$ENTROPY3"
-    time snarkjs zkey verify circuit.r1cs "$PTAU" circuit_0003.zkey
-    time snarkjs zkey beacon circuit_0003.zkey circuit_final.zkey 0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f 10 -n="Final Beacon phase2"
-    time snarkjs zkey verify circuit.r1cs "$PTAU" circuit_final.zkey
+#    time snarkjs zkey contribute circuit_0000.zkey circuit_0001.zkey --name="1st Contribution" -v -e="$ENTROPY1"
+#    time snarkjs zkey contribute circuit_0001.zkey circuit_0002.zkey --name="2nd Contribution" -v -e="$ENTROPY2"
+#    time snarkjs zkey contribute circuit_0002.zkey circuit_0003.zkey --name="3rd Contribution" -v -e="$ENTROPY3"
+#    time snarkjs zkey verify circuit.r1cs "$PTAU" circuit_0003.zkey
+#    time snarkjs zkey beacon circuit_0003.zkey circuit_final.zkey 0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f 10 -n="Final Beacon phase2"
+
+    time snarkjs zkey contribute circuit_0000.zkey circuit_final.zkey --name="1st Contribution" -v -e="$ENTROPY1"
+#    time snarkjs zkey verify circuit.r1cs "$PTAU" circuit_final.zkey
     time snarkjs zkey export verificationkey circuit_final.zkey verification_key.json
     #time snarkjs zkey export json circuit_final.zkey circuit_final.zkey.json
 
@@ -57,7 +59,7 @@ set -u
 
 CIRCUIT="$(pwd)/$1"
 PTAU="$(pwd)/$2"
-PATH="$(pwd)/node_modules/.bin:$PATH"
+#PATH="$(pwd)/node_modules/.bin:$PATH"
 
 # npm ci
 mkdir -p build
